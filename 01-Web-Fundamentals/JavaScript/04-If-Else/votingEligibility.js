fe4041a4fa017b25685fec9807255f8c2b00f9c5
Varun Khatri <varunkhatri950941@gmail.com>
2026-08-01 23:22:21 +0530

    Sprint 1 Day 5 - if else and logical operators

---
+++ 01-Web-Fundamentals/JavaScript/04-If-Else/votingEligibility.js
@@ -0,0 +1,32 @@
+// const name = "Varun";
+// const age = 22;
+
+// if(age>=18){
+//     console.log(`${name} is eligible to vote.`);
+// }
+//     else{
+//         console.log(`${name} is NOT eligible to vote.`);}
+
+
+// const employeeName = "Varun";
+// const experience = 3;
+// const performance = "Excellent";
+
+// if(experience>=2 && performance === "Excellent"){
+//     console.log(`${employeeName} gets the annual bonus.`);
+// }
+// else{
+// console.log(`${employeeName} is not eligible for the annual bonus.`);
+// }
+
+
+const employeeName = "Varun";
+const isManager = false;
+const experience = 6;
+
+if(isManager || experience >=5 ){
+    console.log(`${employeeName} is allowed for remote work`);
+}
+else{
+    console.log(`${employeeName} not allowed for remote work`);
+}
