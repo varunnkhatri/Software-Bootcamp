01041b562da9e3b334b4ac444dd4384088a3c173
Varun Khatri <varunkhatri950941@gmail.com>
2026-08-03 23:03:36 +0530

    Sprint 1 Day 7 - Functions and return values

---
+++ 01-Web-Fundamentals/JavaScript/06-Functions/firstFunction.js
@@ -0,0 +1,34 @@
+function printHeader(title){
+    console.log("======================");
+    console.log(title)
+    console.log("======================");
+}
+
+printHeader("Employee Management");
+printHeader("Student Portal");
+printHeader("Software Bootcamp");
+
+
+
+
+const weight = 90;  
+const height = 1.90;
+function calculateBMI(weight, height) {
+
+const bmi = calculateBMI(90, 1.90);
+console.log(bmi.toFixed(2));
+}
+
+const age = 22;
+function isAdult(age){
+    if (age>=18){
+        return true;
+    }
+return false;
+}
+
+function isAdult(age){
+
+return age>=18;
+
+}
