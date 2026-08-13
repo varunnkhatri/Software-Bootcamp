ad3b6083dd1281a6da2fce9d9e3a85cebb7511cd
Varun Khatri <varunkhatri950941@gmail.com>
2026-08-13 23:58:25 +0530

    Sprint 1 Day 10 - Array Methods Practice

---
+++ 01-Web-Fundamentals/JavaScript/10-DOM/script.js
@@ -0,0 +1,6 @@
+const heading = document.getElementById("heading");
+const button = document.getElementById("change button");
+
+button.addEventListener("Click",function(){
+heading.textContent="Varun";
+});
