6afd6d5173bbd8b1e51b0e6f9ccebba1da9047c8
Varun Khatri <varunkhatri950941@gmail.com>
2026-08-17 23:37:04 +0530

    Practice

--- 01-Web-Fundamentals/JavaScript/10-DOM/script.js
+++ 01-Web-Fundamentals/JavaScript/10-DOM/script.js
@@ -1,7 +1,14 @@
-const heading = document.getElementById("heading");
-const button = document.getElementById("change button");
+// const heading = document.getElementById("heading");
+// const button = document.getElementById("change button");
 
-button.addEventListener("Click",function(){
-heading.textContent="Varun";
-});
+// button.addEventListener("Click",function(){
+// heading.textContent="Varun";
+// });
+
+
+let employeeCount = 0;
+const count = document.getElementById("count");
+const addButton = document.getElementById("addButton");
+const removeButton = document.getElementById("removeButton");
+const resetButton = document.getElementById("resetButton");
 
